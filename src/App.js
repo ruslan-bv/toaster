33416c883b0ebde7cbe4fd2ef2cb130bf465e47d
Ruslan <seitensogekihei@protonmail.ch>
2022-12-06 10:45:46 +0300

    feature: main functionality

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import Snackbar from '@mui/material/Snackbar';
+import Button from '@mui/material/Button';
+import CloseIcon from '@mui/icons-material/Close';
+
+import Header from './Header';
+import Content from './Content';
+
+import { onMessage } from './service/mockServer';
+
+function App() {
+  const [likedSubmissions, setLikedSubmissions] = React.useState([]);
+
+  const [open, setOpen] = React.useState(false);
+  const [currentMessage, setCurrentMessage] = React.useState('');
+  const [currentMessageInfo, setCurrentMessageInfo] = React.useState('');
+
+  const showMessage = (submission) => {
+    setOpen(true);
+    setCurrentMessage(submission)
+    const { data: { email, firstName, lastName } } = submission;
+    setCurrentMessageInfo(`${firstName} ${lastName}` + `${email}`);
+  }
+
+  const handleLikeButton = () => {
+    const likedMessage = { ...currentMessage };
+    likedMessage.data.liked = true;
+    setLikedSubmissions((prevState) => [...prevState, likedMessage]);
+  }
+
+  React.useEffect(() => {
+    onMessage(showMessage);
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <Content />
+        <Snackbar
+          open={open}
+          autoHideDuration={5000}
+          message={currentMessageInfo}
+          action={
+            <>
+              <Button color="inherit" onClick={handleLikeButton} size="small">
+                LIKE
+              </Button>
+              <Button color="inherit" onClick={() => setOpen(false)} size="small">
+                <CloseIcon />
+              </Button>
+            </>
+          }
+        />
+      </Container>
+    </>
+  );
+}
+
+export default App;
